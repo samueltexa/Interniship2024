6cc99f2fc73cb460749e9e31d55543389507f7a5
samueltexa <woolardsamuels@gmail.com>
2024-06-27 22:58:32 +0300

    add Part_One.js

---
+++ JavaScript/Problem_Sets/Variables/Part_One.js
@@ -0,0 +1,24 @@
+// 1. Declare a variable named age and assign your age to it.
+let age = 23;
+// 2. Create a variable named name and set it to your first name.
+let name = "SAMUEL";
+// 3. Declare a variable isStudent and assign it a boolean value true.
+let isStudent = true;
+// 4. Initialize two variables x and y with values 5 and 10, and then create a variable sum that
+// stores the sum of x and y.
+let x = 5;
+let y = 10;
+let sum = x + y;
+// 5. Declare a variable greeting and assign it a string that says "Hello, World!".
+let greeting = "Hello, World!";
+// 6. Create a variable price and set it to 19.99.
+let price = 19.99;
+// 7. Declare a variable isRaining and set it to false.
+let isRaining = false;
+// 8. Initialize a variable color with the value "blue" and then change it to "green".
+let color = "blue";
+color = "green";
+// 9. Create a variable numberOfStudents and set it to 30.
+let numberOfStudents = 30;
+// 10. Declare a variable country and assign it the name of your country.
+let country = "South Africa";
