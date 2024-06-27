3ed311cf0512503c48c066779fb7461b63a1e675
samueltexa <woolardsamuels@gmail.com>
2024-06-27 22:52:01 +0300

    add syntax.js

---
+++ JavaScript/syntax.js
@@ -0,0 +1,19 @@
+// This file contains javascript syntax
+console.log("JavaScript Syntax Update");
+
+
+// This is a comment
+console.log("This is a comment as well!");
+
+
+/*
+altternative for writing comments
+*/
+console.log("This is an alternative comment as well!");
+
+
+let num1 = 10;
+let num2 = 20;
+sum = num1 + num2;
+console.log(sum);
+
