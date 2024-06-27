9aac170a7ced96bcc105bf969da29cc30e85cc76
samueltexa <woolardsamuels@gmail.com>
2024-06-27 22:51:52 +0300

    add loops.js

---
+++ JavaScript/loops.js
@@ -0,0 +1,24 @@
+// Foor loop example
+let numbers = [1,2,3,4,5,6]
+let sum_of_numbers = 0;
+for (let array_index = 0; array_index < numbers.length; array_index++) {
+    console.log(numbers[array_index]);
+}
+
+// Find sum of numbers in an array
+let num = [2,3,4,5,6,7,8,9,10]
+let sum_of_number = 0;
+for (let array_index = 0; array_index < num.length; array_index++) {
+    console.log(num[array_index]);
+    sum_of_number = sum_of_number + num[array_index]
+}
+console.log(sum_of_number)
+
+// While loop example
+let fruits = ["mango", "guav", "apple"]
+let fruit_index = 0;
+while(fruit_index < fruits.length){
+    console.log(fruits[fruit_index]);
+    fruit_index++;
+}
+
