48764b97d2b86df2c0327902322852859d8c3781
samueltexa <woolardsamuels@gmail.com>
2024-07-03 12:33:54 +0300

    change location of several files

--- JavaScript/Challenges/Mutations.js
+++
@@ -1,5 +0,0 @@
-// Mutations
-const mutation = (arr) => {
-    return arr[1].toLowerCase().split("").every(letter => arr[0].toLowerCase().includes(letter));
-}
-console.log(mutation(["hello", "hey"]));
--- JavaScript/Challenges/Repeat_String.js
+++
@@ -1,9 +0,0 @@
-// repeat a string
-const repeatString = (word, num) => {
-    let repeatedString = " ".split("");
-    for (let index = 0; index < num; index++) {
-        repeatedString += word;
-    }
-    return repeatedString;
-};
-console.log(repeatString("hello ", 5));
