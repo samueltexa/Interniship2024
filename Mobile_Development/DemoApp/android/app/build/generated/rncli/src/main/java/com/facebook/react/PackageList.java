df5a268e0c4d2f22eba02c8cd48da0f5d78b3c49
samueltexa <woolardsamuels@gmail.com>
2024-07-16 10:00:26 +0300

    init

--- Mobile_Development/DemoApp/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ Mobile_Development/DemoApp/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,7 +11,16 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
+// @react-native-masked-view/masked-view
+import org.reactnative.maskedview.RNCMaskedViewPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class PackageList {
   private Application application;
@@ -56,7 +65,12 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig)
+      new MainReactPackage(mConfig),
+      new RNCMaskedViewPackage(),
+      new RNGestureHandlerPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new VectorIconsPackage()
     ));
   }
 }
