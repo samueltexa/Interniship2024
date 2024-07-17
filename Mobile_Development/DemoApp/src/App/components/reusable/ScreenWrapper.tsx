48495e70cb3459329fffa3a9be79c8a53f8ff760
samueltexa <woolardsamuels@gmail.com>
2024-07-17 10:13:23 +0300

    add reusable components

--- Mobile_Development/DemoApp/src/App/components/reusable/ScreenWrapper.tsx
+++ Mobile_Development/DemoApp/src/App/components/reusable/ScreenWrapper.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         backgroundColor: 'white',
+        padding: '10%',
     },
     custom_styles: {
         backgroundColor: 'red'
