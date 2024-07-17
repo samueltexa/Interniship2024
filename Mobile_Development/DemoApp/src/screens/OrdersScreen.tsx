3fa5f4c3796190cdf98ea98da1cf96a78f232470
samueltexa <woolardsamuels@gmail.com>
2024-07-18 02:00:33 +0300

    add OrderScreen

---
+++ Mobile_Development/DemoApp/src/screens/OrdersScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const OrdersScreen = () => {
+  return (
+    <View>
+      <Text>OrdersScreen</Text>
+    </View>
+  );
+};
+
+export default OrdersScreen;
+
+const styles = StyleSheet.create({});
