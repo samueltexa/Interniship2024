3f22cb70779f63bb1498fb7046b4ca21e12466d1
samueltexa <woolardsamuels@gmail.com>
2024-07-17 10:11:34 +0300

    add StackNavigator.tsx

--- Mobile_Development/DemoApp/src/App/navigation/StackNavigator.tsx
+++ Mobile_Development/DemoApp/src/App/navigation/StackNavigator.tsx
@@ -4,16 +4,18 @@ import SplashScreen from '../Screens/SplashScreen';
 import LoginScreen from '../Screens/LoginScreen';
 import RegisterScreen from '../Screens/RegisterScreen';
 import WelcomeScreen from '../Screens/WelcomeScreen';
+import BottomTabsNavigator from './BottomTabsNavigator';
 
 const MainStack = createStackNavigator();
 
-export const StackNavigator =()=> {
+export const StackNavigator = () => {
     return (
         <MainStack.Navigator initialRouteName='SplashScreen'>
-            <MainStack.Screen name="SplashScreen" options={{header:()=>null}} component={SplashScreen} />
+            <MainStack.Screen name="SplashScreen" options={{ header: () => null }} component={SplashScreen} />
             <MainStack.Screen name="LoginScreen" component={LoginScreen} />
             <MainStack.Screen name="RegisterScreen" component={RegisterScreen} />
-            <MainStack.Screen name="WelcomeScreen" options={{header:()=>null}} component={WelcomeScreen} />
+            <MainStack.Screen name="WelcomeScreen" options={{ header: () => null }} component={WelcomeScreen} />
+            <MainStack.Screen name="BottomTabNavigator" component={BottomTabsNavigator} />
         </MainStack.Navigator>
     )
-  }
+}
