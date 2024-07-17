4f068f3fdba41eec35f6fef57517a1d5f1c90f1d
samueltexa <woolardsamuels@gmail.com>
2024-07-18 02:02:19 +0300

    add SplashScreen

---
+++ Mobile_Development/DemoApp/src/screens/SplashScreen.tsx
@@ -0,0 +1,34 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import ScreenWrapper from '../components/reusable/ScreenWrapper';
+
+const SplashScreen = ({navigation}: {navigation: any}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('WelcomeScreen');
+    }, 3000);
+  }, []);
+
+  return (
+    <ScreenWrapper
+      custom_styles={{
+        backgroundColor: 'white',
+      }}>
+      <Image
+        style={styles.logo_image}
+        source={require("../assets/images/logo.png")}
+      />
+    </ScreenWrapper>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  logo_image: {
+    width: '40%',
+    height: '40%',
+    objectFit: 'contain',
+    backgroundColor: 'white',
+  },
+});
