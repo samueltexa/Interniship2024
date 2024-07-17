750d82697ea1bc9bbc35d9810bcecaf5738e3ed4
samueltexa <woolardsamuels@gmail.com>
2024-07-17 10:15:38 +0300

    add LoginScreen

--- Mobile_Development/DemoApp/src/App/Screens/LoginScreen.tsx
+++ Mobile_Development/DemoApp/src/App/Screens/LoginScreen.tsx
@@ -1,16 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Image, Text, View } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../components/reusable/ScreenWrapper'
 import RTextInput from '../components/ui/RTextInput'
+import RButton from '../components/reusable/RButton'
+import RText from '../components/reusable/RText'
 
-export default function LoginScreen() {
+export default function LoginScreen({navigation}: {navigation: any }) {
+ 
+
+  const handleLogin = () => {
+    console.log('Login button clicked')
+    navigation.navigate('BottomTabNavigator')
+  }
+
   return (
-    <ScreenWrapper>
-      <RTextInput placeholder='Enter user name'>
+    <ScreenWrapper custom_styles={{
+      backgroundColor: 'red'
+    }}>
+      <Image style={styles.image_wrapper}
+        source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfbd0BJOd5maqKqCpTi7VzAYapgnPm49YQRNHKHvRPmMmyhcHvaYMAJzWObeAj7Iolu5k&usqp=CAU" }} />
+        <RText text='Login from here'></RText>
+      <View style={styles.input_wrapper}>
+      <RTextInput placeholderTextColor={'gray'} placeholder='Enter user name'>
       </RTextInput>
-
+      <RTextInput placeholderTextColor={'gray'} placeholder='Enter password'>
+      </RTextInput>  
+      <RButton onPress={handleLogin} text_content='Login'></RButton>      
+      </View>
     </ScreenWrapper>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input_wrapper: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image_wrapper: {
+    height: 100,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 50,
+    marginBottom: 120,
+  }
+})
