85104409bc392fab942cda9591f80309737bfd65
samueltexa <woolardsamuels@gmail.com>
2024-07-17 10:15:25 +0300

    add CartScreen

---
+++ Mobile_Development/DemoApp/src/App/Screens/CartScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CartScreen() {
+  return (
+    <View>
+      <Text>CartScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
