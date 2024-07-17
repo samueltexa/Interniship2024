17bc6a2772193dcf7b4b519870250249d1230e68
samueltexa <woolardsamuels@gmail.com>
2024-07-18 02:00:51 +0300

    add ProductsScreen

---
+++ Mobile_Development/DemoApp/src/screens/ProductsScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ProductsScreen = () => {
+  return (
+    <View>
+      <Text>ProductsScreen</Text>
+    </View>
+  );
+};
+
+export default ProductsScreen;
+
+const styles = StyleSheet.create({});
